22b1540e40e2fc181a5d54ba8fb7f229f29e1ef2
104072161 <37137908+104072161@users.noreply.github.com>
2018-05-11 17:27:31 +0800

    Add files via upload

--- hw5.py
+++ hw5.py
@@ -1,45 +1,42 @@
 
 # coding: utf-8
 
-# In[4]:
+# In[1]:
 
 
+from collections import Counter
 import csv
 import json
 import pickle
 import string
 
+
 def main(filename):
-    lines =open(filename).readlines()
     all_words = []
-    for line in lines:
-        words=line.split() 
-        for word in words:
-            word=word.strip(string.punctuation)
-            if word!=(""):
-                all_words.append(word) 
+
     
-    from collections import Counter
-    word_counter=Counter(all_words)
-    
-    csv_file=open("wordcount.csv","w")
-    with open("wordcount.csv","w")as csv_file:
-        writer=csv.writer(csv_file)
-        writer.writerow(['word','count'])
-        writer.writerows(word_counter.most_common())
-       
-    with open("wordcount.json","w")as json_file:
-        json.dump(word_counter,json_file)
-        
-    with open("wordcount.pkl","wb")as pkl_file:
-        pickle.dump(word_counter,pkl_file)
-        
-if __name__ == '__main_':
+    for line in open(filename):
+        line = line.strip()
+        if not line:
+            continue
+        for word in line.split():
+            word = word.strip(string.punctuation)
+            if word:
+                all_words.append(word)
+
+    counter = Counter(all_words)
+
+    with open("wordcount.csv", "w") as csv_file:
+        writer = csv.writer(csv_file)
+        writer.writerow(['word', 'count'])
+        writer.writerows(counter.most_common())
+
+    json.dump(list(counter.most_common()), open("wordcount.json", "w"))
+
+
+    pickle.dump(counter, open("wordcount.pkl", "wb"))
+
+
+if __name__ == '__main__':
     main("i_have_a_dream.txt")
-
-
-# In[5]:
-
-
-main("i_have_a_dream.txt")
 
